a8eafc3c262f87794715b4f5745c0452285142c3
miruky <h.miyoki@gmail.com>
2025-12-28 10:01:53 +0900

    libの公開APIをまとめて再エクスポート

---
+++ src/lib/index.ts
@@ -0,0 +1,20 @@
+export {
+  type Dir,
+  type Level,
+  LevelError,
+  DIRECTIONS,
+  DELTA,
+  OPPOSITE,
+  cellOf,
+  xOf,
+  yOf,
+  step,
+  parseLevel,
+  serializeLevel,
+  renderState,
+  levelToCode,
+  levelFromCode,
+} from './level';
+export { Game, type MoveResult } from './game';
+export { solve, deadSquares, type SolveResult, type SolveStatus } from './solver';
+export { BUILTIN_LEVELS, type BuiltinLevel, levelById } from './levels';
